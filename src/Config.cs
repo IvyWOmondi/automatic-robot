34a723750e0987b001161f83567ad5bff2a980c6
Ivy Wanjiku Omondi <omondi581699@newmanu.edu>
2025-09-03 12:25:27 -0500

    Added Config.cs

---
+++ src/Config.cs
@@ -0,0 +1,10 @@
+namespace WeatherApi
+{
+    public static class Config
+    {
+        // Get the key here: https://home.openweathermap.org/api_keys
+        public const string API_KEY = "074bb5ddac5dbb542ae51227ab714427"; // Replace with your actual API key
+        public const string BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+        public const string FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast";
+    }
+}
