f36f06a5c68bb9cd548d2b9f91f89771e493fcee
Ivy Wanjiku Omondi <omondi581699@newmanu.edu>
2025-09-03 12:37:45 -0500

    Deleted 3 .py files

--- src/config.py
+++
@@ -1,5 +0,0 @@
-#Get the key here: https://home.openweathermap.org/api_keys
-API_KEY = "074bb5ddac5dbb542ae51227ab714427"  # Replace with your actual API key
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
-FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast"
-
